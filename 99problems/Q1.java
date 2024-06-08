c7fc43835fdd83914dd417756b31fab8a5c9e50d
Harshit Singh <95492332+singhharshit25@users.noreply.github.com>
2024-06-09 00:34:05 +0530

    Add files via upload

---
+++ 99problems/Q1.java
@@ -0,0 +1,11 @@
+//WAP to read an int value form user and display it 
+
+import java.util.Scanner;
+class Test{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an int value...");
+        int value = sc.nextInt();
+        System.out.println("The input int is: "+value);
+    }
+}
