c1ea989a326116f3dbcb016a6369888a92b201cd
Harshit Singh <95492332+singhharshit25@users.noreply.github.com>
2024-06-10 00:36:43 +0530

    Update Q2.java

--- 99problems/Q2.java
+++ 99problems/Q2.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-class Test {
+class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an int value...");
