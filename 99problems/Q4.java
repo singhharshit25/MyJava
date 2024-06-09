abd418de70bbb77a9a00314a300227924c241e35
Harshit Singh <95492332+singhharshit25@users.noreply.github.com>
2024-06-10 00:37:19 +0530

    Update Q4.java

--- 99problems/Q4.java
+++ 99problems/Q4.java
@@ -1,6 +1,6 @@
 //WAP to read one char value form user and display it.
 import java.util.Scanner;
-class Test {
+class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an char value...");
