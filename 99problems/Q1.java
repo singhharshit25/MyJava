c22bb91ad5ef2f36ad22ad1c5ec4ccd2c2ee6088
Harshit Singh <95492332+singhharshit25@users.noreply.github.com>
2024-06-10 00:36:27 +0530

    Update Q1.java

--- 99problems/Q1.java
+++ 99problems/Q1.java
@@ -1,7 +1,7 @@
 //WAP to read an int value form user and display it 
 
 import java.util.Scanner;
-class Test{
+class Q1{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an int value...");
