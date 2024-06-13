bc1d56c108a7fabd2d8e71c25694014165f03d78
Harshit Singh <95492332+singhharshit25@users.noreply.github.com>
2024-06-13 09:18:36 +0530

    Update Q16.java

--- 99problems/Q16.java
+++ 99problems/Q16.java
@@ -9,17 +9,20 @@ public class Q16 {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
-        if (num2 > num1) {
+        if (num1 > num2) {
             if (num1 > num3) {
                 System.out.println(num1 + " is maximum");
+            } else {
+                System.out.println(num3 + " is maximum");
             }
-         else if (num2 > num1) {
-            if (num2 > num3) {
-                System.out.println(num2 + " is maximum");
-            }}
+        }
+        else if (num2>num3){
+            System.out.println(num2 + " is maximum");
+            
         }
         else{
-            System.out.println(num3+ " is greatest");
+            System.out.println(num3+ " is maximum");
         }
+
     }
 }
