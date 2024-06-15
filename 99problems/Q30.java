a0f4c97f6930671a0409477e9521e7a644f48766
Harshit Singh <95492332+singhharshit25@users.noreply.github.com>
2024-06-15 13:08:12 +0530

    Add files via upload

---
+++ 99problems/Q30.java
@@ -0,0 +1,14 @@
+//wap to read a number and  display first N natural number.
+import java.util.*;
+public class Q30 {
+    public static void main(String[] args) {
+        System.out.println("Enter number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =1;i<=n;i++){
+            System.out.print(i+" ");
+        
+        }
+    }
+    
+}
